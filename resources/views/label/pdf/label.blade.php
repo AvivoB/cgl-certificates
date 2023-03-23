2097cac7958c70daadd577d873d031122fe92988
Avivo <avielber26@gmail.com>
2023-03-23 09:48:41 +0100

    Update label.blade.php

--- resources/views/label/pdf/label.blade.php
+++ resources/views/label/pdf/label.blade.php
@@ -9,7 +9,7 @@
         *{margin:0;padding:1px}
 
         body {
-            font-family: 'Nunito', sans-serif;
+            font-family: 'Arial', sans-serif;
             font-size: 10px;
         }
 
@@ -56,13 +56,30 @@
             display: inline-block;
             width: 30%;
         }
+
+        .label_align {
+            text-align: right;
+            font-weight: 600;
+            line-height: 0.8em;
+            padding-top: 1em;
+        }
+
     </style>
 </head>
 <body>
-    <table style="width: 100%;">
+
+    <div class="label_align">
+        <div>{{ $label->label_num }}</div>
+        <div>{{ Carbon\Carbon::parse($label->created_at)->format('d/m/Y') }}</div>
+        <div>{{ $dataLabel->shape_stone }}</div>
+        <div>{{ $dataLabel->weight }} Ct.</div>
+        <div> <span>Color :</span> <span>{{ $dataLabel->color }}</span> </div>
+        <div> <span>Clarity :</span> <span>{{ $dataLabel->clarity }}</span> </div>
+    </div>
+    {{-- <table style="width: 100%;">
         <tbody>
             <tr align="right">
-                <td></td> <td>{{ $label->label_num }}</td>
+                <td></td> <td></td>
             </tr>
             <tr align="right">
                 <td></td> <td>{{ Carbon\Carbon::parse($label->created_at)->format('d/m/Y') }}</td>
@@ -80,6 +97,6 @@
                 <td align="left">Clarity</td><td>{{ $dataLabel->clarity }}</td>
             </tr>
         </tbody>
-    </table>
+    </table> --}}
 </body>
 </html>
